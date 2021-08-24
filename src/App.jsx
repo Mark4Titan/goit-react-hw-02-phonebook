8aad5b08361ec69e5397c14134449caa1be9a1cb
Mark <52928930+Mark4Titan@users.noreply.github.com>
2021-08-24 23:31:05 +0300

    adds build

--- src/App.jsx
+++ src/App.jsx
@@ -1,168 +1,84 @@
-import React from 'react'
-// import { v4 as uuidv4 } from 'uuid'
-// import './App.css'
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import ContactForm from './ContactForm'
+import ContactList from './ContactList'
+import Filter from './Filter'
+import './App.css'
 
 const INITIAL_STATE = {
-	login: '',
-	email: '',
-	password: '',
+	contacts: [
+		{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+		{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+		{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+		{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+	],
+	filter: '',
+	name: '',
+	number: '',
 }
 
 class App extends React.Component {
 	state = { ...INITIAL_STATE }
 
-	handleChange = ({ target }) => {
-		const { name, value } = target
-
-		this.setState({ [name]: value })
+	formSubmitHandler = data => {
+		this.repeatControl(data)
 	}
 
-	handleSubmit = evt => {
-		evt.preventDefault()
-
-		const { login, email, password } = this.state
-
-		console.log(`
-      Login: ${login}
-      Email: ${email}
-      Password: ${password}
-    `)
-
-		this.props.onSubmit({ ...this.state })
-		this.reset()
+	repeatControl = data => {
+		let nameArray = this.state.contacts.map(cur => cur.name)
+		if (!nameArray.includes(data.name)) {
+			let arrayCont = [
+				...this.state.contacts,
+				{ id: uuidv4(), name: data.name, number: data.number },
+			]
+			return this.setState({ ...this.state, contacts: arrayCont })
+		} else {
+			alert(' Контакт вже є у телефонній книзі!!!')
+		}
 	}
 
-	reset = () => {
-		this.setState({ ...INITIAL_STATE })
+	elementDelete = (arr, idContact) => {
+		return arr.filter(elem => elem.id !== idContact)
 	}
 
-	render() {
-		const { login, email, password } = this.state
+	deleteContactFromContactList = idContact => {
+		let newArrAfterDel = this.elementDelete(this.state.contacts, idContact)
+		this.setState({
+			...this.state,
+			contacts: [...newArrAfterDel],
+		})
+	}
 
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<label>
-						Name
-						<input
-							type="text"
-							placeholder="Enter login"
-							name="login"
-							value={login}
-							onChange={this.handleChange}
-						/>
-					</label>
-					<label>
-						Email
-						<input
-							type="email"
-							placeholder="Enter email"
-							name="email"
-							value={email}
-							onChange={this.handleChange}
-						/>
-					</label>
-					<label>
-						Password
-						<input
-							type="password"
-							placeholder="Enter password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-						/>
-					</label>
+	setFilterToState = filterData => {
+		this.setState({ ...this.state, filter: `${filterData}` })
+	}
 
-					<button type="submit">Sign up as {login}</button>
-				</form>
-			</div>
+	filterArr = filterArr => {
+		return filterArr.filter(cur =>
+			cur.name.toUpperCase().includes(this.state.filter)
 		)
 	}
 
-	// state = {
-	// 	contacts: [
-	// 		// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-	// 		// { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-	// 		// { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-	// 		// { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-	// 	],
-	// 	filter: '',
-	// 	name: '',
-	// 	number: '',
-	// }
-	// handleChange = ({ target }) => {
-	// 	const { name, value } = target
-	// 	this.setState({ [name]: value })
-	// }
-	// handleSubmit = evt => {
-	// 	evt.preventDefault()
-	// 	const { contacts, name, number } = this.state
-	// 	console.log(`
-	//   contacts: ${contacts}
-	//   number: ${number}
-	// `)
-	// 	this.props.onSubmit({ ...this.state })
-	// 	this.reset()
-	// }
-	// render() {
-	// 	const { contacts, name, number, filter } = this.state
-	// 	return (
-	// 		<div className="PhoneBookBlock">
-	// 			<form onSubmit={this.handleSubmit} className="phonebookBox">
-	// 				<label className="PhoneBook">
-	// 					<p>PhoneBook</p>
-	// 					<div id={uuidv4()} className="inputText">
-	// 						<p>Name</p>
-	// 						<input
-	// 							type="text"
-	// 							name="contacts"
-	// 							placeholder="Enter name"
-	// 							value={contacts}
-	// 							onChange={this.handleChange}
-	// 							pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-	// 							title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-	// 							required
-	// 						/>
-	// 					</div>
-	// 				</label>
-	// 				{/* <div id={uuidv4()} className="inputText">
-	// 					<p>Nmaber</p>
-	// 					<input
-	// 						type="tel"
-	// 						name="number"
-	// 						placeholder="Enter number"
-	// 						value={number}
-	// 						onChange={this.handleChange}
-	// 						pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-	// 						title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-	// 						required
-	// 					/>
-	// 				</div> */}
-	// 				<div>
-	// 					<button className="addContact" type="submit">
-	// 						Add contact {contacts}
-	// 					</button>
-	// 				</div>
-	// 			</form>
-	// 			{/* <div className="PhoneBookContacts">
-	// 				<h2>Contacts</h2>
-	// 				<p>Find contacts by name</p>
-	// 				<input
-	// 					type="Find"
-	// 					name="Find"
-	// 					pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-	// 					title="Поиск"
-	// 					required
-	// 				/>
-	// 				<ul>
-	// 					<li>
-	// 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-	// 					</li>
-	// 				</ul>
-	// 			</div> */}
-	// 		</div>
-	// 	)
-	// }
+	render() {
+		const { contacts } = this.state
+
+		return (
+			<div di="PhoneBookBlock" className="PhoneBookBlock">
+				<div className="Contact">
+					<p>PhoneBook</p>
+					<ContactForm onSubmitData={this.formSubmitHandler} />
+				</div>
+				<div className="Contact Pad">
+					<h1>Contacts</h1>
+					<Filter setFilterToState={this.setFilterToState} />
+					<ContactList
+						contacts={this.filterArr(contacts)}
+						del={this.deleteContactFromContactList}
+					/>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App
